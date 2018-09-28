cf7db11fd7a93f9c078822f0b6dfbc43592b76c7
mary lu <xhkj@xhkjdemac-mini.local>
2018-09-28 10:09:47 +0800

    Initial Commit

---
+++ popK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  popK
+//
+//  Created by xhkj on 2018/9/28.
+//  Copyright © 2018年 JDKJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ popK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  popK
+//
+//  Created by xhkj on 2018/9/28.
+//  Copyright © 2018年 JDKJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
